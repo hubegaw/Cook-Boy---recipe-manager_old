7f87319886e92faf8423e09c1e7ef122060c1317
Hubert Gawczyński <hubert.gawczynski@gmail.com>
2022-12-18 17:11:30 +0100

    layout simplified

---
+++ public/views/footer.php
@@ -0,0 +1,15 @@
+<div class="footer flexbox">
+    <ul class="flexbox">
+        <li>
+            <a href="#">About Us</a>
+        </li>
+        <li>
+            <span>Contact:</span>
+            <span class="email">cookboyrecipes@contact.com</span>
+        </li>
+        <li>
+            <a href="#">Help</a>
+        </li>
+    </ul>
+    <span class="legal">@2022 All rights reserved</span>
+</div>
